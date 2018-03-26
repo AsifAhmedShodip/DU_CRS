99fe655c400ab7fa71f2d6bdc275709e3de627c0
deathstroke44 <saminyeaserlight@gmail.com>
2018-03-26 14:56:12 +0600

    Omi

---
+++ app/src/main/java/ExamHallSlot.java
@@ -0,0 +1,83 @@
+/**
+ * Created by aniomi on 3/26/18.
+ */
+
+public class ExamHallSlot {
+    public ExamHallSlot(int startTime, int endTime, int reservetionId, String reserverId, boolean isCancelled) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.reservetionId = reservetionId;
+        this.reserverId = reserverId;
+        this.isCancelled = isCancelled;
+    }
+
+    public ExamHallSlot() {
+    }
+
+    int startTime;
+    int endTime;
+    int counter;
+
+    public ExamHallSlot(int startTime, int endTime, int counter, int reservetionId, String reserverId, boolean isCancelled) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.counter = counter;
+        this.reservetionId = reservetionId;
+        this.reserverId = reserverId;
+        this.isCancelled = isCancelled;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(int endTime) {
+        this.endTime = endTime;
+    }
+
+    public void setReservetionId(int reservetionId) {
+        this.reservetionId = reservetionId;
+    }
+
+    public void setReserverId(String reserverId) {
+        this.reserverId = reserverId;
+    }
+
+    public void setCancelled(boolean cancelled) {
+        isCancelled = cancelled;
+    }
+
+    public int getStartTime() {
+        return startTime;
+
+    }
+
+    public int getEndTime() {
+        return endTime;
+    }
+
+    public int getReservetionId() {
+        return reservetionId;
+    }
+
+    public String getReserverId() {
+        return reserverId;
+    }
+
+    public boolean isCancelled() {
+        return isCancelled;
+    }
+
+    int reservetionId;
+    String reserverId;
+    boolean isCancelled;
+
+}
---
+++ app/src/main/java/FunctionList.java
@@ -0,0 +1,54 @@
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by aniomi on 3/26/18.
+ */
+
+public class FunctionList {
+    static int getminute(int hourss,int minutess)
+    {
+        return hourss*60+minutess;
+    }
+    static boolean isTheTimeSlotFree(int st, int ed,int capacity,int need, ArrayList<ExamHallSlot> Bookedslots)
+    {
+        int flag[]=new int[2000];
+        for(int i=0;i<2000;i++)
+        {
+            flag[i]=capacity;
+        }
+        int l=Bookedslots.size();
+        for(int i=0;i<l;i++)
+        {
+            int stt=Bookedslots.get(i).startTime;
+            int ett=Bookedslots.get(i).endTime;
+            int count=Bookedslots.get(i).counter;
+            for(int j=stt;j<=ett;j++)
+            {
+                flag[j]-=count;
+            }
+        }
+        for(int i=st;i<=ed;i++)
+        {
+            if(flag[i]<need) return false;
+        }
+        return true;
+    }
+    static <Type> List<Type> createListFromFirebase(Type temp,Object obj,DataSnapshot dataSnapshot) throws ClassNotFoundException {
+        obj.getClass();
+        List<Type> list=new ArrayList<>();
+
+        for(DataSnapshot users : dataSnapshot.getChildren())
+        {
+            Class cl=obj.getClass();
+            obj=users.getValue(obj.getClass());
+            list.add((Type) obj);
+        }
+        return list;
+    }
+
+}
