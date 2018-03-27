60801811cdc740135669486a74ce85ecc22f94c8
deathstroke44 <saminyeaserlight@gmail.com>
2018-03-27 21:49:13 +0600

    lkl

--- app/src/main/java/com/example/omi/du_crs/SearchActivity.java
+++ app/src/main/java/com/example/omi/du_crs/SearchActivity.java
@@ -1,28 +1,82 @@
 package com.example.omi.du_crs;
 
+import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
-import android.icu.util.Calendar;
+
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TimePicker;
 
 import com.example.asif.du_crs.R;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import in.galaxyofandroid.spinerdialog.OnSpinerItemClick;
 import in.galaxyofandroid.spinerdialog.SpinnerDialog;
 
 public class SearchActivity extends AppCompatActivity {
-    EditText search_category,start_time,end_time,st_date,ed_date,department,room_no,ground;
-    Button search_button;
-    SpinnerDialog choice_dialogue;
+    Spinner search_category,dept,auditorioum,ground;
+    String final_choice,final_dept,final_audi,final_ground;
+    EditText stb,etb,sdb,edb,room_no;
     ArrayList<String> choice=new ArrayList<>();
+    ArrayList<String> audis=new ArrayList<>();
+    ArrayList<String> depts=new ArrayList<>();
+    ArrayList<String> grounds=new ArrayList<>();
+    void init_all()
+    {
+        stb.setVisibility(View.GONE);
+        etb.setVisibility(View.GONE);
+        sdb.setVisibility(View.GONE);
+        edb.setVisibility(View.GONE);
+        dept.setVisibility(View.GONE);
+        auditorioum.setVisibility(View.GONE);
+        room_no.setVisibility(View.GONE);
+        ground.setVisibility(View.GONE);
+    }
+    void set_all()
+    {
+        stb.setVisibility(View.VISIBLE);
+        etb.setVisibility(View.VISIBLE);
+        sdb.setVisibility(View.VISIBLE);
+        edb.setVisibility(View.VISIBLE);
+        dept.setVisibility(View.VISIBLE);
+        auditorioum.setVisibility(View.VISIBLE);
+        room_no.setVisibility(View.VISIBLE);
+        ground.setVisibility(View.VISIBLE);
+    }
+    void init_lab_class()
+    {
+        set_all();
+        auditorioum.setVisibility(View.GONE);
+        ground.setVisibility(View.GONE);
+    }
+    void init_ground()
+    {
+        set_all();
+        dept.setVisibility(View.GONE);
+        room_no.setVisibility(View.GONE);
+        auditorioum.setVisibility(View.GONE);
+    }
+    void init_audi()
+    {
+        set_all();
+        dept.setVisibility(View.GONE);
+        room_no.setVisibility(View.GONE);
+        ground.setVisibility(View.GONE);
+    }
+    void init_exam_hall()
+    {
+        set_all();
+    }
     void init()
     {
         choice.add("Exam Hall");
@@ -30,22 +84,12 @@ public class SearchActivity extends AppCompatActivity {
         choice.add("Lab Room");
         choice.add("Ground");
         choice.add("Auditorioum");
-    }
-    void class_lab_init()
-    {
-        department.setVisibility(View.VISIBLE);
-        room_no.setVisibility(View.VISIBLE);
-        ground.setVisibility(View.GONE);
-    }
-    void ground_auditorioum_init()
-    {
-        department.setVisibility(View.GONE);
-        room_no.setVisibility(View.GONE);
-        ground.setVisibility(View.VISIBLE);
-    }
-    void exam_hall_init()
-    {
-
+        depts.add("CSE");
+        depts.add("EEE");
+        audis.add("TSC");
+        audis.add("Cinet bhobon");
+        grounds.add("Swimming pool");
+        grounds.add("Hagi mohsin");
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,82 +97,76 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
         init();
         search_category=findViewById(R.id.search_category);
-        start_time=findViewById(R.id.start_time);
-        end_time=findViewById(R.id.end_time);
-        st_date=findViewById(R.id.lower_date);
-        ed_date=findViewById(R.id.higher_date);
-        department=findViewById(R.id.department);
+        dept=findViewById(R.id.deptName);
+        auditorioum=findViewById(R.id.auditorioum);
+        ground=findViewById(R.id.Ground);
+        stb=findViewById(R.id.stb);
+        etb=findViewById(R.id.etb);
+        sdb=findViewById(R.id.sdb);
+        edb=findViewById(R.id.edb);
         room_no=findViewById(R.id.room_no);
-        ground=findViewById(R.id.ground);
-        search_button=findViewById(R.id.search_buttons);
-
-        choice_dialogue=new SpinnerDialog(getParent(),choice,"Enter Category");
-        choice_dialogue.bindOnSpinerListener(new OnSpinerItemClick() {
+        init_exam_hall();
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,choice);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        search_category.setAdapter(adapter);
+        search_category.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(String s, int i) {
-                search_category.setText(s);
-                if(i==0) exam_hall_init();
-                if(i==1 || i==2) class_lab_init();
-                if(i==3 || i==4) ground_auditorioum_init();
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                final_choice=adapterView.getItemAtPosition(i).toString();
+                if(i==0) init_exam_hall();
+                if(i==1 || i==2) init_lab_class();
+                if(i==3) init_ground();
+                if(i==4) init_audi();
             }
-        });
-        search_category.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View view) {
-                choice_dialogue.showSpinerDialog();
-            }
-        });
-        search_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(search_category.getText().equals("Exam Hall"))
-                {
-
-                }
-                if(search_category.getText().equals("Class Room"))
-                {
-
-                }
-                if(search_category.getText().equals("Lab Room"))
-                {
-
-                }
-                if(search_category.getText().equals("Ground"))
-                {
-
-                }
-                if(search_category.getText().equals("Auditorioum"))
-                {
-
-                }
-            }
-        });
-        start_time.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+            public void onNothingSelected(AdapterView<?> adapterView) {
 
             }
         });
-        end_time.setOnClickListener(new View.OnClickListener() {
+        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,depts);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dept.setAdapter(adapter);
+        dept.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                final_dept=adapterView.getItemAtPosition(i).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
 
             }
         });
-        st_date.setOnClickListener(new View.OnClickListener() {
+        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,audis);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        auditorioum.setAdapter(adapter);
+        auditorioum.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                final_audi=adapterView.getItemAtPosition(i).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
 
             }
         });
-        ed_date.setOnClickListener(new View.OnClickListener() {
+        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,grounds);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ground.setAdapter(adapter);
+        ground.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                final_ground=adapterView.getItemAtPosition(i).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
 
             }
         });
-        start_time.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.N)
+        stb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Calendar mcurrentTime = Calendar.getInstance();
@@ -164,14 +202,13 @@ public class SearchActivity extends AppCompatActivity {
                             choosedMinute= ""+selectedMinute;
                         }
 
-                        start_time.append(" "+choosedHour + ":" + choosedMinute +" "+choosedTimeZone);
+                        stb.setText(" "+choosedHour + ":" + choosedMinute +" "+choosedTimeZone);
                     }
                 }, hour, minute, false);
                 mTimePicker.show();
             }
         });
-        end_time.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.N)
+        etb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Calendar mcurrentTime = Calendar.getInstance();
@@ -207,12 +244,15 @@ public class SearchActivity extends AppCompatActivity {
                             choosedMinute= ""+selectedMinute;
                         }
 
-                        end_time.append(" "+choosedHour + ":" + choosedMinute +" "+choosedTimeZone);
+                        etb.setText(" "+choosedHour + ":" + choosedMinute +" "+choosedTimeZone);
                     }
                 }, hour, minute, false);
                 mTimePicker.show();
             }
         });
+
+
+
 
     }
     //
