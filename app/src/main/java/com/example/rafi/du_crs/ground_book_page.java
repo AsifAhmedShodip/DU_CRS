ff996e850e0f9420443b5bda0b891441dc1ee270
ShadmanRafi <shadman.islam.rafi@gmail.com>
2018-04-24 14:14:49 +0600

    ground

---
+++ app/src/main/java/com/example/rafi/du_crs/ground_book_page.java
@@ -0,0 +1,15 @@
+package com.example.rafi.du_crs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.asif.du_crs.R;
+
+public class ground_book_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ground_book_page);
+    }
+}
